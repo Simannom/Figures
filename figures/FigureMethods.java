9b2c4fb9cf05dc5292f050632388ff943449c556
Simannom <simannom@yandex.ru>
2018-07-02 17:35:27 +0300

    Update FigureMethods.java

--- figures/FigureMethods.java
+++ figures/FigureMethods.java
@@ -1,4 +1,4 @@
-package figure;
+package figures;
 
 
 import java.awt.*;
@@ -8,16 +8,12 @@ import java.awt.*;
  */
 public interface FigureMethods {
 
-    //метод перемещения
     void move(Node changeVector);
 
-    //метод вывода на экран
     void show(Graphics2D g, int width, int height);
 
-    //метод масштабирования
     void scale(double multiplier, Node centerPoint);
 
-    //площадь
     Double area();
 
 }
